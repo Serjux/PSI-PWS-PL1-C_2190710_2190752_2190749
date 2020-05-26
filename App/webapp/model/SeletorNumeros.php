4cea469475531e27c7257631cff86ce7409fdc91
Sérgio <36546740+Serjux@users.noreply.github.com>
2020-05-26 09:56:57 +0100

    Criada a classe SeletorNumeros.

---
+++ App/webapp/model/SeletorNumeros.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class SeletorNumeros
+{
+    private $numerosSelecionados = array();
+
+    public function validateNumber($userNumber, $numerosBloqueio) {
+
+    }
+
+    public function updateSelection($userNumber) {
+
+    }
+
+    public function checkSelectionTotal($totalDados) {
+
+    }
+
+    public function clearSelection() {
+
+    }
+
+    public function selectionHasNumber($number) {
+
+    }
+}
