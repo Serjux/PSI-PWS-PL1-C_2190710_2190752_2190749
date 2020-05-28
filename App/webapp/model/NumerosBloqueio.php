9a0edc368c4cfb2986ad9e32b33dc27165d66258
Sérgio <36546740+Serjux@users.noreply.github.com>
2020-05-28 13:08:06 +0100

    Começado o código nas classes SeletorNumeros e NumerosBloqueio.

--- App/webapp/model/NumerosBloqueio.php
+++ App/webapp/model/NumerosBloqueio.php
@@ -4,9 +4,17 @@
 class NumerosBloqueio
 {
     private $numerosBloqueio;
+    public $seletorNumeros;
+
+    public function __construct() {
+        $this->seletorNumeros = new SeletorNumeros();
+        $this->iniciar();
+    }
 
     public function iniciar() {
-
+        for($i = 1; $i <= 9; $i++) {
+            $this->numerosBloqueio[''.$i] = false;
+        }
     }
 
     public function bloquearNumeros($numeros, $somaDados) {
--- App/webapp/model/SeletorNumeros.php
+++ App/webapp/model/SeletorNumeros.php
@@ -6,22 +6,44 @@ class SeletorNumeros
     private $numerosSelecionados = array();
 
     public function validateNumber($userNumber, $numerosBloqueio) {
-
+        if($numerosBloqueio[$userNumber] == true) {
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 
     public function updateSelection($userNumber) {
-
+        if(in_array($userNumber, $this->numerosSelecionados) == true) {
+            // Eliminar o usernumber do vetor this->numerosSelecionados
+        }
+        else {
+            array_push($this->numerosSelecionados);
+        }
     }
 
     public function checkSelectionTotal($totalDados) {
+        $soma = 0;
 
+        foreach($this->numerosSelecionados as $numero) {
+            $soma += $numero;
+        }
+
+        if($soma == $totalDados) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public function clearSelection() {
-
+        // Remover todos os elementos do vetor this->numerosSelecionados
     }
 
     public function selectionHasNumber($number) {
-
+        // Usar este método na vista, para sabermos qual o link a colocar no botão
+        return in_array($number, $this->numerosSelecionados);
     }
 }
